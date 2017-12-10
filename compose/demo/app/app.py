8939af4f97572fa041f2d89bcd37017cc00e12fb
khs1994 <khs1994@khs1994.com>
2017-12-10 11:52:09 +0800

    Update compose #288

---
+++ compose/demo/app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
+@app.route('/')
+def hello():
+    count = redis.incr('hits')
+    return 'Hello World! 该页面已被访问 {} 次。\n'.format(count)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
